01b61bf44bc62c7e0816aebd732f86c319993a49
Amber Jones <amberlovescats14@gmail.com>
2019-09-19 09:08:44 -0500

    areaOfATriangle

--- warmups/morning-warmUp.js
+++ warmups/morning-warmUp.js
@@ -1,3 +1,9 @@
 
 const func = (a, b) => a + b;
 func(1,2);
+//5, 6, 7
+const findAreaOfTriangle =( a, b, c) => {
+    let p = (a + b + c) / 2
+    return Math.sqrt(p(p-a)(p-b)(p-c))
+}
+findAreaOfTriangle(5,6,7)
