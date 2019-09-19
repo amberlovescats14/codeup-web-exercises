2864b6c24630f3e8cd102791c28ecb2fb0bcb735
Amber Jones <amberlovescats14@gmail.com>
2019-09-19 09:17:56 -0500

    p is not a functiono, it needed to have multiplaction symbols in it

--- warmups/morning-warmUp.js
+++ warmups/morning-warmUp.js
@@ -1,9 +1,10 @@
+//  node warmups/morning-warmUp.js
 
 const func = (a, b) => a + b;
 func(1,2);
 //5, 6, 7
 const findAreaOfTriangle =( a, b, c) => {
     let p = (a + b + c) / 2
-    return Math.sqrt(p(p-a)(p-b)(p-c))
+    return Math.sqrt(p*(p-a)*(p-b)*(p-c))
 }
-findAreaOfTriangle(5,6,7)
+console.log(findAreaOfTriangle(5,6,7));
