b359458b132896a0257cfbebfc11796a051ef3c0
Amber Jones <amberlovescats14@gmail.com>
2019-10-07 14:06:53 -0500

    functions with prompts

--- js/external.js
+++ js/external.js
@@ -37,14 +37,55 @@ alert(`The amount for your rentals is ${amountDueForRentals(moviesRented)}`)
 let myCompanies = [
     google = {
         pay: 400,
-        time: 6
+        time: Number(prompt(`How many hours did you work? (Google)`))
     },
     amazon = {
         pay: 380,
-        time: 4
-    },
+        time: Number(prompt(`How many hours did you work? (Amazon)`))    },
     facebook = {
         pay: 350,
-        time: 10
-    }
+        time: Number(prompt(`How many hours did you work? (Facebook)`))    }
 ];
+const calculateWeeklyPay = arr => {
+    let pay = 0;
+    arr.forEach(a => {
+        pay += a.time * a.pay
+    })
+    return pay
+}
+alert(`Your pay this week is ${calculateWeeklyPay(myCompanies)}`)
+
+//________________________class availability
+
+let classSize = 30
+let numberOfStudents = 21
+const spaceAvailable = () => {
+    if(numberOfStudents >= classSize) alert(`This class is full`)
+    let conflicting = prompt(`Does this class conflict with your current schedule? (y/n)`)
+    if(conflicting.toLowerCase() === 'y') alert(`This class conflicts with your current schedule`)
+    else alert(`Registration complete.`)
+
+}
+spaceAvailable()
+
+//_---------------login
+const login = {
+    username: prompt(`Username: `),
+    password: prompt(`Password: `)
+}
+
+const verification = (obj) => {
+    const { username, password } = obj;
+    let noWhiteSpace = new RegExp(/^\s|\s$/)
+    
+    if(password.length >= 5)
+        if(password.indexOf(username) < 0)
+            if(username.length <= 20)
+                if(!noWhiteSpace.test(username) && !noWhiteSpace.test(password))  alert(`Login Successful`)
+    else alert(`Incorrect Username or Password`)
+    
+
+}
+verification(login)
+
+
